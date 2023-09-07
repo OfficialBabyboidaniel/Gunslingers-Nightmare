1cf8c97381fb26305232b0162438dabece926867
Daniel Olsson <125807379+OfficialBabyboidaniel@users.noreply.github.com>
2023-09-07 12:46:04 +0200

    lägg in ai enemy i level 1, plus skapa prefabs

--- Gunslingers Nightmare/Assets/AIFootstepController.cs
+++ Gunslingers Nightmare/Assets/AIFootstepController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class AIFootstepController : MonoBehaviour
 {
@@ -9,21 +10,35 @@ public class AIFootstepController : MonoBehaviour
     public float maxDistance = 10f; // The maximum distance at which footsteps can be heard
     public AudioSource footstepAudioSource; // Reference to the AudioSource on the enemy
 
+    public AudioMixer audioMixer;
+
     private void Update()
     {
-        // Calculate the distance between the enemy and the player
-        float distanceToPlayer = Vector3.Distance(transform.position, player.position);
+        Vector3 directionToPlayer = player.position - transform.position;
+
+        // Calculate the angle between the forward direction of the enemy and the direction to the player
+        float angle = Vector3.SignedAngle(transform.forward, directionToPlayer, Vector3.up);
+
+        // Normalize the angle to be between -180 and 180 degrees
+        angle = Mathf.Clamp(angle, -180f, 180f);
+
+        // Calculate the panStereo value based on the angle (adjust this based on your desired effect)
+        float panStereo = angle / 180f;
+
+        // Set the panStereo value to the AudioSource
+        footstepAudioSource.panStereo = panStereo;
 
         // Calculate the volume based on the distance (closer = louder, farther = quieter)
-        float volume = 1f - (distanceToPlayer / maxDistance);
+        float volume = 100f - (directionToPlayer.magnitude / maxDistance);
 
         // Clamp the volume to be between 0 and 1
-        volume = Mathf.Clamp01(volume);
+        
+        audioMixer.SetFloat("Master", volume);
 
         // Set the volume of the enemy's footsteps
         footstepAudioSource.volume = volume;
 
-        // You can also play the footstep sound here if it's not already playing
+        // Play the footstep sound if it's not already playing
         if (!footstepAudioSource.isPlaying)
         {
             footstepAudioSource.Play();
