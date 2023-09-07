3c6910f3c71268d7e899fa376b66b9557355137e
Daniel Olsson <125807379+OfficialBabyboidaniel@users.noreply.github.com>
2023-09-07 14:44:36 +0200

    vidare fix med auidio for Ai

--- Gunslingers Nightmare/Assets/Scripts/AIChase.cs
+++ Gunslingers Nightmare/Assets/Scripts/AIChase.cs
@@ -1,23 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
+
 
 public class AIChase : MonoBehaviour
 {
     public GameObject player;
     public float speed;
     private float distance;
+    private bool canMove;
     // Start is called before the first frame update
     void Start()
     {
-
+        canMove = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        distance = Vector2.Distance(transform.position, player.transform.position);
-        Vector2 direction = player.transform.position - transform.position;
-        transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
+        if (canMove)
+        {
+            distance = Vector2.Distance(transform.position, player.transform.position);
+            Vector2 direction = player.transform.position - transform.position;
+            transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
+        } 
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            // If the collision is with an object tagged as "Player,"
+            Debug.Log("ccollion hit player");
+            canMove = false;
+        }
     }
 }
