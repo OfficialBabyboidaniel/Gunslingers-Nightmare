09f2f06819d2b200f9b2a3e1006ceef43e818b05
Daniel Olsson <125807379+OfficialBabyboidaniel@users.noreply.github.com>
2023-09-27 15:45:24 +0200

    build

--- Gunslingers Nightmare/Assets/Scripts/AiMaxDistanceTool.cs
+++ Gunslingers Nightmare/Assets/Scripts/AiMaxDistanceTool.cs
@@ -3,24 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 
-[CustomEditor(typeof(AIChase))]
-public class AiMaxDistanceTool : Editor
-{
-    private void OnSceneGUI()
-    {
-        AIChase aiChase = (AIChase)target;
-        Handles.color = Color.red;
-        Handles.DrawWireDisc(aiChase.transform.position, Vector3.back, aiChase.MaxSightRange);
+// [CustomEditor(typeof(AIChase))]
+// public class AiMaxDistanceTool : Editor
+// {
+//     private void OnSceneGUI()
+//     {
+//         AIChase aiChase = (AIChase)target;
+//         Handles.color = Color.red;
+//         Handles.DrawWireDisc(aiChase.transform.position, Vector3.back, aiChase.MaxSightRange);
 
-        EditorGUI.BeginChangeCheck();
-        float newSightRange = Handles.RadiusHandle(Quaternion.identity, aiChase.transform.position, aiChase.MaxSightRange);
+//         EditorGUI.BeginChangeCheck();
+//         float newSightRange = Handles.RadiusHandle(Quaternion.identity, aiChase.transform.position, aiChase.MaxSightRange);
 
-        if (EditorGUI.EndChangeCheck())
-        {
-            // Ensure the sight range is not negative
-            newSightRange = Mathf.Max(0, newSightRange);
-            aiChase.MaxSightRange = newSightRange;
-            EditorUtility.SetDirty(aiChase);
-        }
-    }
-}
+//         if (EditorGUI.EndChangeCheck())
+//         {
+//             // Ensure the sight range is not negative
+//             newSightRange = Mathf.Max(0, newSightRange);
+//             aiChase.MaxSightRange = newSightRange;
+//             EditorUtility.SetDirty(aiChase);
+//         }
+//     }
+// }
