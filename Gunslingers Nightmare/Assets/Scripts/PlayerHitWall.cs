69a2b59ae5931ef226322944b29e6ddf0b250a7a
Liam-1234 <104007472+Liam-1234@users.noreply.github.com>
2023-09-27 11:16:19 +0200

    Hit wall sound 2.0

--- Gunslingers Nightmare/Assets/Scripts/PlayerHitWall.cs
+++ Gunslingers Nightmare/Assets/Scripts/PlayerHitWall.cs
@@ -5,13 +5,31 @@ using UnityEngine;
 public class PlayerHitWall : MonoBehaviour
 {
 
-    [SerializeField] private AudioSource source;
-    [SerializeField] private AudioClip clip;
+    // [SerializeField] private AudioSource source;
+
+    [SerializeField] private GameObject soundObject;
+    private AudioSource source;
+    [SerializeField] private AudioClip hitClip;
+
+    // private Rigidbody2D rb;
+
+    void Start() {
+        source = soundObject.GetComponent<AudioSource>();
+        // rb = GetComponent<Rigidbody2D>();
+    }
 
     void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.CompareTag("Wall")) {
-            source.PlayOneShot(clip);
+            source.PlayOneShot(hitClip);
         }
     }
+
+    /*
+    void OnCollisionStay2D(Collision2D collision) {
+        if (collision.gameObject.CompareTag("Wall") && rb.velocity.magnitude > 1.0f && playNextClip) {
+            source.PlayOneShot(hitClip);
+        }
+    }
+    */
 }
