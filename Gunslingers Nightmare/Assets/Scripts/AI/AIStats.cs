443409ead224d1baaf078622f7608aa7f297cf81
Daniel Olsson <125807379+OfficialBabyboidaniel@users.noreply.github.com>
2023-10-11 12:47:47 +0200

    ai chase sound bug fix
    chase ljud hörs igen nu

--- Gunslingers Nightmare/Assets/Scripts/AI/AIStats.cs
+++ Gunslingers Nightmare/Assets/Scripts/AI/AIStats.cs
@@ -8,17 +8,6 @@ public class AIStats : MonoBehaviour
     public int HitDamage = 4;
     public float attackCooldown = 3f; // Cooldown time in seconds
     public float MaxSightRange = 10.0f;
-
     public float hitRange = 1.0f;
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
+    
 }
