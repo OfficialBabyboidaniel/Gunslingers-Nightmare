35138d61f0cf71f0f7e65b3c82d85f8b10b3674b
Daniel Olsson <125807379+OfficialBabyboidaniel@users.noreply.github.com>
2023-10-19 18:13:59 +0200

    bugfix

--- Gunslingers Nightmare/Assets/Scripts/Player/PlayerStats.cs
+++ Gunslingers Nightmare/Assets/Scripts/Player/PlayerStats.cs
@@ -9,33 +9,26 @@ public class PlayerStats : MonoBehaviour
     // Start is called before the first frame update
 
     public int playerHP = 30;
-
-    public AudioClip DeathClip;
-
     public AudioSource audioSource;
-
+    public AudioClip DeathClip;
     private bool isDead = false;
 
     // Update is called once per frame
     void Update()
     {
-        
+
 
         if (playerHP <= 0 && !isDead)
         {
+            // audio source is disabled bug?? fattar inte. make no sense 
             Debug.Log("player is dead");
-
             audioSource.PlayOneShot(DeathClip);
-            
+
             Renderer renderer = GetComponent<Renderer>();
             if (renderer != null)
             {
                 renderer.enabled = false;
             }
-
-            Destroy(gameObject, DeathClip.length);
-
-            Time.timeScale = 0f;
 
             isDead = true;
 
